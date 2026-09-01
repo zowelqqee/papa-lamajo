fcc60e816641f62b88eb6081d9f33d6377b5fb88
Arseniy Abramidze <zowel.aep@mail.ru>
2026-09-01 15:36:57 +0300

    lala

--- lib/content.ts
+++ lib/content.ts
@@ -381,7 +381,7 @@ export const menu: MenuGroup[] = [
         variants: ["зелёный", "чебрец", "молочный улун", "мята"],
         price: "350 ₽",
         src: "/images/green-tea.jpg",
-        imageClassName: "object-[center_40%]",
+        imageClassName: "object-[center_65%]",
       },
       {
         slug: "chay-chernyy",
@@ -390,7 +390,7 @@ export const menu: MenuGroup[] = [
         variants: ["чёрный", "чёрный с бергамотом", "фруктовый"],
         price: "350 ₽",
         src: "/images/black-tea.jpg",
-        imageClassName: "object-[center_40%]",
+        imageClassName: "object-[center_65%]",
       },
       {
         slug: "kofe",
